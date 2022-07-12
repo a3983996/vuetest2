c30cc80170b93aef3940d2bd2d364200658b91fc
kai <a3983996@gmail.com>
2022-07-12 20:07:29 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,20 @@
     <!-- <LoadingComponent :active="isLoading"></LoadingComponent> -->
     <NavbarComponent :isWhiteNav="isWhiteNav"></NavbarComponent>
     <router-view @push-isWhiteNav="isWhiteNav = $event" />
-    <FooterComponent></FooterComponent>
+    <!-- <FooterComponent></FooterComponent> -->
   </div>
 </template>
 
 <script>
 // import LoadingComponent from "./components/LoadingComponent.vue";
 import NavbarComponent from "./components/NavbarComponent.vue";
-import FooterComponent from "./components/FooterComponent.vue";
+// import FooterComponent from "./components/FooterComponent.vue";
 import { ref } from "vue";
 export default {
   components: {
     // LoadingComponent,
     NavbarComponent,
-    FooterComponent,
+    // FooterComponent,
   },
   setup() {
     const isLoading = ref(true);
